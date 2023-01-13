ab35cf1793aa727618cfa7d36d222d0b3d48ddd8
GioRotoloS <101062886+GioRotoloS@users.noreply.github.com>
2023-01-13 09:06:07 -0400

    Add files via upload

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,13 +1,13 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
-import  Jwt  from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
 export const register = (req, res)=>{
     //Check if the user already exist
 
     const q = "SELECT * FROM nodeblog.users WHERE email = ? OR username = ?";
 
-    db.query(q, [req.body.email, req.body.name], (err, data)=>{
+    db.query(q, [req.body.email, req.body.username], (err, data)=>{
         if(err)  return res.json(err);
         
         if(data.length) return res.status(409).json("User already exist");
@@ -17,7 +17,7 @@ export const register = (req, res)=>{
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
-        const q = "INSERT INTO nodeblog.users(username, email,password) VALUE (?);"
+        const q = "INSERT INTO nodeblog.users(username, email, password) VALUE (?);"
         const values = [
             req.body.username,
             req.body.email,
@@ -37,32 +37,30 @@ export const login = (req,res)=>{
 
     const q = "SELECT * FROM nodeblog.users WHERE username = ?";
 
-    db.query(q, [req.body.name], (err, data) => {
+    db.query(q, [req.body.username], (err, data) => {
         if (err) return res.json(err);
         if (data.length === 0) return res.status(404).json("User not found!");
 
         //Check password
-        const isPasswordCorrect = bcrypt.compareSync(
-            req.body.password,
-            data[0].password
-        );
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
 
         if(!isPasswordCorrect)
             return res.status(400).json("Wrong username or password!");
 
-            const token = jwt.sign({ id: data[0].id}, "jwtkey");
-            const { password, ...other } = data[0];
+        const token = jwt.sign({ id: data[0].id}, "jwtkey");
+        const { password, ...other } = data[0];
 
-            res
-                .cookie("acces_token", token, {
-                    httpOnly: true,
-                })
-                .status(200)
-                .json(other);
+        res.cookie("access_token", token, {
+                httpOnly: true,
+             }).status(200).json(other);
     });
 
 }
 
 export const logout = (req,res)=>{
 
+    res.clearCookie("access_token",{
+        sameSite:"none",
+        secure:true
+    }).status(200).json("User has been logged out.")
 }

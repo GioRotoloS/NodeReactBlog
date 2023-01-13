3269d394ee9d2b285c7a0ff65e8d34863cf56eb6
GioRotoloS <101062886+GioRotoloS@users.noreply.github.com>
2023-01-13 09:08:29 -0400

    Add files via upload

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png"
+
+const Navbar = () => {
+
+    const { currentUser, logout } = useContext(AuthContext);
+    return(
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={Logo} alt="" />
+                    </Link>
+                    
+                </div>
+                <div className="links">
+                    <Link className="links" to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className="links" to="/?cat=science">
+                        <h6>SCIENCE</h6>
+                    </Link>
+                    <Link className="links" to="/?cat=technology">
+                        <h6>TECHNOLOGY</h6>
+                    </Link>
+                    <Link className="links" to="/?cat=cinema">
+                        <h6>CINEMA</h6>
+                    </Link>
+                    <Link className="links" to="/?cat=design">
+                        <h6>DESIGN</h6>
+                    </Link>
+                    <Link className="links" to="/?cat=food">
+                        <h6>FOOD</h6>
+                    </Link>
+                    <span>{currentUser?.username}</span>
+                    {currentUser?(
+                        <span onClick={logout}>
+                            <a href="/login">Logout</a>
+                            </span>
+                    ) : (
+                        <Link className="link" to="/login">
+                            Login
+                        </Link>
+                    )}
+                    <span className="Write">
+                        <Link className="link" to="/write">
+                            Write
+                        </Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
